e0496479d6d26caeac576064ae50969a80e94678
Shashank3173 <163388187+Shashank3173@users.noreply.github.com>
2025-07-30 10:36:22 +0530

    Update asdf.groovy

--- vars/asdf.groovy
+++ vars/asdf.groovy
@@ -1,6 +1,10 @@
-def call(){
-  sh ''' mkdir -p todo echo "this is the shared file from github and used in two pipelines and using it in 30th july pipeline too" | tee -a todo/new.txt'''
-  // sh 'echo "this is the shared file from github and used in two pipelines and using it in 30th july pipeline too" | tee todo/new.txt'
-  echo "today's jenkins work is to set up the agent"
-  echo"todays golang works is to practice all the previous stuffs and work on go routines channels mutex enums and start with the api"
+def call() {
+    sh '''
+        mkdir -p todo
+        echo "this is the shared file from github and used in two pipelines and using it in 30th july pipeline too" | tee -a todo/new.txt
+    '''
+
+    echo "today's jenkins work is to set up the agent"
+
+    echo "todays golang works is to practice all the previous stuffs and work on go routines channels mutex enums and start with the api"
 }
